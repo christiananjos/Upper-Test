16054e6353e05a39c13bc00c104a2885d9eb601b
Christian_Anjos <christiananjos@hotmail.com>
2020-11-23 14:24:01 -0300

    Add Interface IRepository

---
+++ Business/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Filter(Func<T, bool> predicate);
+        void SaveChanges();
+    }
+}
