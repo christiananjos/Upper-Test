4a7f5edf4cdd562326b896a4aced532790493e62
Christian_Anjos <christiananjos@hotmail.com>
2020-11-23 18:27:31 -0300

    add new interface IBusiness

---
+++ Business/Interfaces/IBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Interfaces
+{
+    public interface IBusiness<T> where T : class
+    {
+        void Cria(T entity);
+        void Apaga(T entity);
+        void Edita(T entity);
+        T RetornaPorId(int id);
+        IEnumerable<T> BuscaTodos();
+    }
+}
